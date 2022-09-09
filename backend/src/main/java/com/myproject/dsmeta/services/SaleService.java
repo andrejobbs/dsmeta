ea22da905144bc4c2113d8340daec86a51f008b6
André Jonas <andrejonaspesceromanoli@gmail.com>
2022-09-09 10:32:36 -0300

    Date Select

--- backend/src/main/java/com/myproject/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/myproject/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.myproject.dsmeta.services;
 
-import java.util.List;
+
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.myproject.dsmeta.entities.Sale;
@@ -11,8 +17,14 @@ import com.myproject.dsmeta.repositories.SaleRepository;
 public class SaleService {
 	@Autowired
 	private SaleRepository repository;
-	public List <Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate,Pageable pageable) {
+		
+		LocalDate today=LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min= minDate.equals("")? today.minusDays(365): LocalDate.parse(minDate);
+		LocalDate max= maxDate.equals("")? today: LocalDate.parse(maxDate);
+		
+		return repository.findSales(min,max,pageable);
 		
 	}
 
